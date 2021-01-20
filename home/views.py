38337c5028c4fd2b26dc284f9f69955598472700
Rodrigo <rodrigocosta47@outlook.com>
2021-01-20 00:07:08 -0300

    Corrigi o problem das migrations

--- home/views.py
+++ home/views.py
@@ -4,4 +4,13 @@ from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def home(req):
-    return render(req, "index.html")
+    if req.method == 'POST':
+        form = AuthenticationForm(data=req.POST)
+        if form.is_valid():
+            form.save()
+            #o usuário está logado
+            return redirect('novo_template')
+    else:
+        # Se o usuário não estiver logado ele rotorna uma instancia do form vazio
+        form = AuthenticationForm()
+    return render(req, "index.html", {'form': form})
