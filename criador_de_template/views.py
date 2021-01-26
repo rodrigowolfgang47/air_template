4934766f67759dff4707f115ea830ad98fe4dc7c
Rodrigo <rodrigocosta47@outlook.com>
2021-01-26 20:26:52 -0300

    Adicieio cometarios as views

--- criador_de_template/views.py
+++ criador_de_template/views.py
@@ -7,21 +7,27 @@ import pandas as pd
 
 
 # Create your views here.
+
+# Coloquei o decorator login_request para obrigar o usuário a logar
 @login_required()
 def crie_seu_template(request):
     return render(request, 'crie_seu_template.html')
 
 
+#View responsável por deslogar usuário
 def sair(request):
+    #pego a request e passo para o logout
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
 
+#View responsável por gerar o download
 def download_file(request):
     file_path = "criador_de_template/staticfiles/excel/base_air_template_layout.xlsx"
     file_name = 'base_air_template_layout.xlsx'
 
+    #Adicionei a lib pandas para abrir a planilha e a openpyxl para erencias os arquivos em excel
     file = pd.read_excel(file_path)
     mime_type, _ = mimetypes.guess_type(file_path)
     response = HttpResponse(file, content_type=mime_type)
