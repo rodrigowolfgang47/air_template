43fe0e168c42f7d5d661caeb963848499a4b9299
Rodrigo <rodrigocosta47@outlook.com>
2021-01-26 15:31:25 -0300

    Implementei do download do arquivo em excel

--- criador_de_template/url.py
+++ criador_de_template/url.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import crie_seu_template, sair
+from .views import crie_seu_template, sair, download_file
 
 urlpatterns = [
     path('crie_seu_template/', crie_seu_template, name='cria-template'),
-    path('logout/', sair, name='sair')
+    path('logout/', sair, name='sair'),
+    path('staticfiles/excel/base_air_template_layout.xlsx', download_file, name='download')
 ]
--- criador_de_template/views.py
+++ criador_de_template/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import mimetypes
+import pandas as pd
 
 
 # Create your views here.
@@ -8,7 +11,19 @@ from django.shortcuts import render, redirect
 def crie_seu_template(request):
     return render(request, 'crie_seu_template.html')
 
+
 def sair(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+
+def download_file(request):
+    file_path = "criador_de_template/staticfiles/excel/base_air_template_layout.xlsx"
+    file_name = 'base_air_template_layout.xlsx'
+
+    file = pd.read_excel(file_path)
+    mime_type, _ = mimetypes.guess_type(file_path)
+    response = HttpResponse(file, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; file_name=%s" % file_name
+    return response
