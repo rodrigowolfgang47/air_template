e0454852678cfc302c3605f265a4f606084bc801
Rodrigo <rodrigocosta47@outlook.com>
2021-02-05 17:26:39 -0300

    Criei o front-end da tela de login

--- administracao_clientes/views.py
+++ administracao_clientes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from criador_de_template.models import Cliente
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def meus_templates(request):
     lista_de_clientes = Cliente.objects.all().order_by('-criacao')
 
-    paginador = Paginator(lista_de_clientes, 3)
+    paginador = Paginator(lista_de_clientes, 12)
     pagina = request.GET.get('page')
 
     clientes = paginador.get_page(pagina)
