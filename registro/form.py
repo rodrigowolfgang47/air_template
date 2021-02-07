2706e5680afa3b58dd803b93778a8d6af8050069
Rodrigo <rodrigocosta47@outlook.com>
2021-02-07 12:10:29 -0300

    Adicionei placeholder aos campos no formularios

--- registro/form.py
+++ registro/form.py
@@ -12,3 +12,21 @@ class Cadastro(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name',
 'email', 'password1', 'password2',)
+
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Usuario'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'Nome'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Sobrenome'}),
+            'email': forms.TextInput(attrs={'placeholder': 'E-mail'}),
+            'password1': forms.TextInput(attrs={'placeholder': 'Senha'}),
+            'password2': forms.TextInput(attrs={'placeholder': 'Repita a senha'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(Cadastro, self).__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuário'})
+        self.fields['first_name'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'})
+        self.fields['last_name'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Sobrenome'})
+        self.fields['email'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'})
+        self.fields['password1'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Senha'})
+        self.fields['password2'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Confirme sua senha'})
