7f8de1b2ecf3b2deba451432d57d5588f7854a74
Rodrigo <rodrigocosta47@outlook.com>
2021-02-16 10:58:10 -0300

    Novos banco

--- air_template/settings.py
+++ air_template/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'air_template.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'postgresql')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
